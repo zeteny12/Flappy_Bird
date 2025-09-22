5cd07322158cfee119fa57a3848795113fc480a8
zeteny12 <zetus0512@gmail.com>
2025-09-22 23:06:39 +0200

    bg speed and fps changed

---
+++ Flappy_Bird.py
@@ -0,0 +1,39 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+clock = pygame.time.Clock()
+fps = 100
+
+screen_width = 464
+screen_height = 626
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Flappy Bird')
+
+#define game variables
+bg_scroll = 0
+scroll_speed = 1
+
+#load images
+bg = pygame.image.load('img/Flappy_Bird_bg.png')
+
+run = True
+while run:
+
+    clock.tick(fps)
+
+    #draw background
+    screen.blit(bg, (bg_scroll,-260))
+    bg_scroll -= scroll_speed
+    if abs(bg_scroll) > 908:
+        bg_scroll = 0
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
